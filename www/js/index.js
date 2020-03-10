601030de9b87d3d5a7fd270adfb11a0b1163a250
FGiordanengo <florent.giordanengo@gmail.com>
2020-03-10 17:00:51 +0100

    ajout leaflet

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,11 @@
 console.log('starting application');
+
 var watchPositionId = null;
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
 document.addEventListener('deviceready', function(event) {
     console.log('device is ready');
 
@@ -15,6 +21,17 @@ document.addEventListener('deviceready', function(event) {
             console.log(geoEvent);
             // alert('Tu es géolocalisé à la latitude : ' +geoEvent.coords.latitude+ 'et la longitude :'+geoEvent.coords.longitude);
     
+            var marker = L.marker([geoEvent.coords.latitude, geoEvent.coords.longitude]).addTo(mymap);
+
+            var circle = L.circle([geoEvent.coords.latitude, geoEvent.coords.longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: geoEvent.coords.accuracy
+            }).addTo(mymap);
+
+            mymap.setView([geoEvent.coords.latitude, geoEvent.coords.longitude], 10);
+
             document.querySelector('.latitude').innerHTML = geoEvent.coords.latitude;
             document.querySelector('.longitude').innerHTML = geoEvent.coords.longitude;
             document.querySelector('.accuracy').innerHTML = geoEvent.coords.accuracy;
